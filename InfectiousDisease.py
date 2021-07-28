a9cb41c3eb785025f68796b102f8ce39bd768172
Radian Lukman <radianlukman20@gmail.com>
2021-07-28 15:26:26 +0700

    add plot function

--- InfectiousDisease.py
+++ InfectiousDisease.py
@@ -1,57 +1,48 @@
-# Impporting libraries
+# Importing libraries
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Read the dataset as pandas dataframe "data".
 data = pd.read_csv("PenyakitMenularDKI.csv")
 print(data)
 print(data.info())
 
+# Change year, region, and disease column type into category 
 data['tahun']=data['tahun'].astype('category')
 data['wilayah']=data['wilayah'].astype('category')
 data['nama_penyakit']=data['nama_penyakit'].astype('category')
 print(data.info())
 
-# Pada tahun berapa penyakit menular tertinggi?
-# Plot ini dibuat menggunakan pandas plot dan digrup berdasaran tahun untuk mendapatkan total kasus tiap tahun.
 pertahun = data.groupby('tahun').jumlah_penderita.sum()
 print(pertahun)
 pertahun.plot(kind='line',xlabel='Tahun',ylabel='Jumlah',title='Jumlah Penderita Penyakit Menular di DKI Jakarta \n  2015-2020')
 
-# Penyakit menular apa yang terbanyak dari tahun 2015-2020
-# Plot ini menggunakann pandas
-penyakit = data.groupby('nama_penyakit').jumlah_penderita.sum().sort_values()
-print(penyakit)
-penyakit.plot(kind='barh',xlabel='Nama Penyakit',title='Jumlah Penderita Penyakit Menular di DKI Jakarta \n Menurut Jenis Penyakit')
+def PlotGrup(grup,judul):
+    data_a = data.groupby(grup).jumlah_penderita.sum().sort_values()
+    print(data_a)
+    data_a.plot(kind="barh",title=judul)
+    
+PlotGrup(grup='nama_penyakit',judul='Jumlah Penderita Penyakit Menular di DKI Jakarta Berdasarkan Jenis Penyakit')
+PlotGrup(grup='wilayah',judul="Jumlah Penderita Penyakit Menular di DKI Jakarta Berdasarkan Wilayah")
 
-# Menghitung Jumlah Penderita Penyakit Menular berdasarkan wilayahnnya.
-wilayah = data.groupby('wilayah').jumlah_penderita.sum().sort_values()
-print(wilayah)
-wilayah.plot(kind='barh',xlabel='Kabupaten/Kota Administrasi', title='Jumlah Penderita Penyakit Menular di DKI Jakarta \n Menurut Kabupaten/Kota Administrasi')
+def PlotGrid(baris, kolom, judul):
+    sns.set_style('darkgrid')
+    g1=sns.catplot(x=baris,y='jumlah_penderita', col=kolom, data=data, kind='bar',ci=None,col_wrap=3,height=5,sharey=False)
+    g1.fig.subplots_adjust(top=0.9)
+    g1.fig.suptitle(judul,fontsize=19)
+    g1.set_xticklabels(rotation=30)
+    plt.show()
+    plt.clf()
 
 # Membuat plot Penderita penyakit pertahun
-sns.set_style('darkgrid')
-g1 = sns.catplot(x='nama_penyakit',y='jumlah_penderita',col='tahun',data=data,kind='bar',ci=None,col_wrap=3,height=5,sharey=False,sharex=False)
-g1.fig.subplots_adjust(top=0.9) 
-g1.fig.suptitle('Jumlah Penderita Penyakit per tahun',fontsize=19)
-plt.show()
-plt.clf()
-# Dapat dilihat bahwa tiap tahun, yang mendominasi penyakit menular di DKI Jakarta adalah TBC, disusul oleh Gastro Entritits lalu DBD.
+PlotGrid(baris='nama_penyakit',kolom='tahun',judul="Jumlah Penderita Penyakit per Tahun")
 
 # Membuat plot penderita penyakit tiap wilayah pertahun.
-g3 = sns.catplot(x='wilayah',y='jumlah_penderita',col='tahun',data=data,kind='bar',ci=None,col_wrap=3,height=5,sharey=False)
-g3.set_xticklabels(rotation=30)
-g3.fig.subplots_adjust(top=0.9) 
-g3.fig.suptitle('Jumlah Penderita Tiap Wilayah Per Tahun',fontsize=19)
-# Dapat dilihat bahwa tiap tahun, wilayah administrasi yang memiliki jumlah penderita penyakit menular terbanyak adalah Jakarta Timur, disusul oleh Jakarta Barat dan Jakarta Selatan.
+PlotGrid(baris='wilayah',kolom='tahun',judul='Jumlah Penderita Tiap Wilayah per Tahun')
 
-# Penderita penyakit per wilayah administrasi
-sns.set_style('darkgrid')
-g2 = sns.catplot(x='nama_penyakit',y='jumlah_penderita',col='wilayah',data=data,kind='bar',ci=None,col_wrap=3,height=5,sharey=False,sharex=False)
-g2.set_xlabels(fontsize=15)
-g2.fig.subplots_adjust(top=0.9) 
-g2.fig.suptitle('Jumlah Penderita Penyakit per Wilayah Administrasi',fontsize=19)
+# Membuat plot jenis penyakit per wilayah.
+PlotGrid(baris='nama_penyakit',kolom='wilayah',judul='Jumlah Penderita Penyakit Tiap Wilayah')
 
 # Plot trend penyakit tiap tahun
 tren = pd.pivot_table(data, index='tahun', columns='nama_penyakit', values='jumlah_penderita')
